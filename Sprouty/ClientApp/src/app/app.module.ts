9cd1c09c43d515e81d7c73cec98e7ff354a899a3
Mr.L <81031706+HaotianLi666@users.noreply.github.com>
2021-11-04 17:00:11 -0400

    Component Update
    Update and formatting App component

--- Sprouty/ClientApp/src/app/app.module.ts
+++ Sprouty/ClientApp/src/app/app.module.ts
@@ -1,9 +1,12 @@
+//Modules
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
 import { HttpClientModule } from '@angular/common/http';
-import { RepositoryService } from './services/repository.service';
+import { RouterModule } from '@angular/router';
+import { AppRoutingModule } from './app.routing';
+//Components
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './components/nav-menu/nav-menu.component';
 import { HomeComponent } from './components/home/home.component';
@@ -11,11 +14,12 @@ import { FooterComponent } from './components/footer/footer.component';
 import { RegisterComponent } from './components/register/register.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component'
 import { AlertComponent } from './components/alert';
-import { LoginComponent } from './components/login';
-import { AppRoutingModule } from './app.routing';
-import { RouterModule } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
 import { MyplantsComponent } from './components/myplants/myplants.component';
 import { AddPlantsComponent } from './components/myplants/add-plants/add-plants.component';
+import { ButtonComponent } from './components/myplants/button/button.component';
+//Services
+import { RepositoryService } from './services/repository.service';
 
 
 @NgModule({
@@ -32,7 +36,8 @@ import { AddPlantsComponent } from './components/myplants/add-plants/add-plants.
     RegisterComponent,
     DashboardComponent,
     MyplantsComponent,
-    AddPlantsComponent
+    AddPlantsComponent,
+    ButtonComponent
 
   ],
   imports: [
@@ -49,7 +54,7 @@ import { AddPlantsComponent } from './components/myplants/add-plants/add-plants.
       { path: 'login', component: LoginComponent },
       { path: 'myplants', component: MyplantsComponent },
       { path: 'dashboard', component: DashboardComponent },
-
+      { path: 'add-plants', component: AddPlantsComponent},
     ])
   ],
   providers: [
