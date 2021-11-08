6694d6a98e4dfa659bc2b74fa9db07046df87f79
Mr.L <81031706+HaotianLi666@users.noreply.github.com>
2021-11-08 10:50:13 -0500

    Updated Dashboard to store 3 plants per line

--- Sprouty/ClientApp/src/app/components/dashboard/dashboard.component.ts
+++ Sprouty/ClientApp/src/app/components/dashboard/dashboard.component.ts
@@ -7,63 +7,58 @@ import { Component, OnInit} from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
+  Users = [
+    [
+      {
+        "id": 1,
+        "img_url": "https://cb.scene7.com/is/image/Crate/JadePlantSSS21/$web_pdp_main_carousel_med$/210212144233/artificial-potted-jade-plant.jpg",
+        "name": "Faux Potted Jade Plant",
+        "view": "View",
+        "boolean" : ""
+      },
+      {
+        "id": 2,
+        "img_url": "https://cb.scene7.com/is/image/Crate/PottedMonstera24inSHS20/$web_pdp_main_carousel_high$/191217102319/faux-potted-monstera.jpg",
+        "name": "Faux Potted Monstera",
+        "view": "View",
+        "boolean" : ""
+      },
+      {
+        "id": 3,
+        "img_url": "https://cb.scene7.com/is/image/Crate/PottedZZPlantSHS20/$web_pdp_main_carousel_high$/191217102319/potted-zz-plant.jpg",
+        "name": "Faux Potted ZZ Plant",
+        "view": "View",
+        "view_url":"https://www.google.com/",
+        "boolean" : "false"
+      }
+      
+    ],
+    [
+      {
+        "id": 4,
+        "img_url": "https://cb.scene7.com/is/image/Crate/JadePlantSSS21/$web_pdp_main_carousel_med$/210212144233/artificial-potted-jade-plant.jpg",
+        "name": "Faux Potted Jade Plant",
+        "view": "View",
+        "boolean" : ""
+      },
+      {
+        "id": 5,
+        "img_url": "https://cb.scene7.com/is/image/Crate/PottedMonstera24inSHS20/$web_pdp_main_carousel_high$/191217102319/faux-potted-monstera.jpg",
+        "name": "Faux Potted Monstera",
+        "view": "View",
+        "boolean" : ""
+      },
+      {
+        "id": 6,
+        "img_url": "https://cb.scene7.com/is/image/Crate/PottedZZPlantSHS20/$web_pdp_main_carousel_high$/191217102319/potted-zz-plant.jpg",
+        "name": "Faux Potted ZZ Plant",
+        "view": "View",
+        "view_url":"https://www.google.com/",
+        "boolean" : "false"
+      }
+   ]
+  ]
   
-Users = [
-  [
-    {
-      "id": 1,
-      "img_url": "https://cb.scene7.com/is/image/Crate/JadePlantSSS21/$web_pdp_main_carousel_med$/210212144233/artificial-potted-jade-plant.jpg",
-      "name": "Faux Potted Jade Plant",
-      "view": "View",
-      "boolean" : ""
-    },
-    {
-      "id": 2,
-      "img_url": "https://cb.scene7.com/is/image/Crate/PottedMonstera24inSHS20/$web_pdp_main_carousel_high$/191217102319/faux-potted-monstera.jpg",
-      "name": "Faux Potted Monstera",
-      "view": "View",
-      "boolean" : ""
-    },
-    {
-      "id": 3,
-      "img_url": "https://cb.scene7.com/is/image/Crate/PottedZZPlantSHS20/$web_pdp_main_carousel_high$/191217102319/potted-zz-plant.jpg",
-      "name": "Faux Potted ZZ Plant",
-      "view": "View",
-      "view_url":"https://www.google.com/",
-      "boolean" : "false"
-    }
-    
-  ],
-  [
-    {
-      "id": 1,
-      "img_url": "https://cb.scene7.com/is/image/Crate/JadePlantSSS21/$web_pdp_main_carousel_med$/210212144233/artificial-potted-jade-plant.jpg",
-      "name": "Faux Potted Jade Plant",
-      "view": "View",
-      "boolean" : ""
-    },
-    {
-      "id": 2,
-      "img_url": "https://cb.scene7.com/is/image/Crate/PottedMonstera24inSHS20/$web_pdp_main_carousel_high$/191217102319/faux-potted-monstera.jpg",
-      "name": "Faux Potted Monstera",
-      "view": "View",
-      "boolean" : ""
-    },
-    {
-      "id": 3,
-      "img_url": "https://cb.scene7.com/is/image/Crate/PottedZZPlantSHS20/$web_pdp_main_carousel_high$/191217102319/potted-zz-plant.jpg",
-      "name": "Faux Potted ZZ Plant",
-      "view": "View",
-      "view_url":"https://www.google.com/",
-      "boolean" : "false"
-    }
- ]
-]
-
-
-  isNextLine:boolean = false;
-  decimal: number = 0;
-
   constructor() 
   { 
 
